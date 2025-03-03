0ed80e254a6d0c29f267b308d1c2016db90fd4f6
VictoriaLa <laskava@gmail.com>
2025-03-02 19:28:24 -0500

    Full game model synchronisation: part II

--- heartslav.js
+++ heartslav.js
@@ -255,6 +255,12 @@ define([
 
           var card_id = items[0].id;
           console.log("on playCard " + card_id);
+          // type is (color - 1) * 13 + (value - 2)
+          var type = items[0].type;
+          var color = Math.floor(type / 13) + 1;
+          var value = type % 13 + 2;
+          
+          this.playCardOnTable(this.player_id,color,value,card_id);
 
           this.playerHand.unselectAll();
         } else if (this.checkAction("actGiveCards")) {

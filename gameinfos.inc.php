8127cfc8988823832ac39a3551083d5d618ff39c
Victoria La <laskava@gmail.com>
2025-10-22 21:10:08 -0400

    templ 3

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -14,19 +14,19 @@ $gameinfos = [
     'game_name' => "My Great Game",
 
     // Game publisher (use empty string if there is no publisher)
-    'publisher' => 'My Publishing Company',
+   'publisher' => 'Public Domain',
 
     // Url of game publisher website
     'publisher_website' => 'http://www.mypublishingcompany.com/',
 
     // Board Game Geek ID of the publisher
-    'publisher_bgg_id' => 1234,
+   'publisher_bgg_id' => 171,
 
     // Board game geek ID of the game
     'bgg_id' => 54321,
 
     // Players configuration that can be played (ex: 2 to 4 players)
-    'players' => [2, 3, 4],
+    'players' => [4],
 
     // Suggest players to play with this number of players. Must be null if there is no such advice, or if there is only one possible player configuration.
     // NB: the automatic lobby will try first the lowest number of players if this is not specified. So you _have to_ specify this parameter if the lowest player number is not compatible with the default options.
